4dcd28c93c0a67e09999e2c7e28f1b725fe44494
Dietrich Daroch <dietr1ch@acm.org>
2024-11-28 16:31:36 -0300

    Testing: Add parsing tests

---
+++ tests/parse.rs
@@ -0,0 +1,56 @@
+use cs2_annotations::map;
+
+// TODO: Consider using https://docs.rs/assert_ok
+macro_rules! assert_ok {
+    ($expression:expr) => {
+        match $expression {
+            Ok(_) => (),
+            ref e => panic!("Expected Ok(_) but got `{:?}`", e),
+        }
+    };
+}
+
+#[test]
+fn parse_ancient() {
+    assert_ok!(map::Annotation::read_annotations(map::Name::Ancient));
+}
+
+#[test]
+fn parse_anubis() {
+    assert_ok!(map::Annotation::read_annotations(map::Name::Anubis));
+}
+
+#[test]
+fn parse_dust2() {
+    assert_ok!(map::Annotation::read_annotations(map::Name::Dust2));
+}
+
+#[test]
+fn parse_inferno() {
+    assert_ok!(map::Annotation::read_annotations(map::Name::Inferno));
+}
+
+#[test]
+fn parse_mirage() {
+    assert_ok!(map::Annotation::read_annotations(map::Name::Mirage));
+}
+
+#[test]
+fn parse_nuke() {
+    assert_ok!(map::Annotation::read_annotations(map::Name::Nuke));
+}
+
+#[test]
+fn parse_overpass() {
+    assert_ok!(map::Annotation::read_annotations(map::Name::Overpass));
+}
+
+#[test]
+fn parse_train() {
+    assert_ok!(map::Annotation::read_annotations(map::Name::Train));
+}
+
+#[test]
+fn parse_vertigo() {
+    assert_ok!(map::Annotation::read_annotations(map::Name::Vertigo));
+}
